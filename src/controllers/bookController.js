b1661f24f5cea7646c84f53fc65d14ab7039e262
romanpoudel <romanpoudel325@gmail.com>
2023-08-11 11:36:46 +0545

    get books and search

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,65 +1,101 @@
+import { Op } from "sequelize";
 import bookModel from "../models/bookModel.js";
 
 export default class BookController {
-  async addBook(req, res, imageName) {
-    const data = await bookModel.create({ ...req.body, image: imageName });
-    console.log(data);
-    if (data) {
-      res.json(data);
-    } else {
-      res.json({ success: false, message: "Error during adding the book." });
-    }
-  }
+	async addBook(req, res, imageName) {
+		const data = await bookModel.create({ ...req.body, image: imageName });
+		console.log(data);
+		if (data) {
+			res.json(data);
+		} else {
+			res.json({
+				success: false,
+				message: "Error during adding the book.",
+			});
+		}
+	}
 
-  async getBookByID(req, res) {
-    const { id } = req.params;
-    if (id) {
-      const data = await bookModel.findByPk(id);
-      if (data) {
-        res.json(data);
-      } else {
-        res.json([]);
-      }
-    } else {
-      res.json({ success: false, message: "Book ID not provided." });
-    }
-  }
+	async getBookByID(req, res) {
+		const { id } = req.params;
+		if (id) {
+			const data = await bookModel.findByPk(id);
+			if (data) {
+				res.json(data);
+			} else {
+				res.json([]);
+			}
+		} else {
+			res.json({ success: false, message: "Book ID not provided." });
+		}
+	}
 
-  async updateBook(req, res) {
-    const { id } = req.params;
-    if (id) {
-      const data = await bookModel.update(req.body, {
-        where: {
-          id,
-        },
-      });
-      if (data[0] === 1) {
-        res.json({ success: true, message: "Updated Book" });
-      } else {
-        res.json({ success: false, message: "Couldn't Update Book" });
-      }
-    } else {
-      res.json({ success: false, message: "Book ID not provided." });
-    }
-  }
+	async updateBook(req, res) {
+		const { id } = req.params;
+		if (id) {
+			const data = await bookModel.update(req.body, {
+				where: {
+					id,
+				},
+			});
+			if (data[0] === 1) {
+				res.json({ success: true, message: "Updated Book" });
+			} else {
+				res.json({ success: false, message: "Couldn't Update Book" });
+			}
+		} else {
+			res.json({ success: false, message: "Book ID not provided." });
+		}
+	}
 
-  async deleteBook(req, res) {
-      const { id } = req.params;
-      console.log(id)
-    if (id) {
-      const data = await bookModel.destroy({
-        where: {
-          id,
-        },
-      });
-      console.log(data);
-      if (data === 1) {
-        res.json({ success: true, message: "Book Deleted" });
-      } else {
-        res.json({ success: false, message: "Couldn't Delete Book" });
-      }
-    } else {
-      res.json({ success: false, message: "Book ID not provided." });
-    }
-  }
+	async deleteBook(req, res) {
+		const { id } = req.params;
+		console.log(id);
+		if (id) {
+			const data = await bookModel.destroy({
+				where: {
+					id,
+				},
+			});
+			console.log(data);
+			if (data === 1) {
+				res.json({ success: true, message: "Book Deleted" });
+			} else {
+				res.json({ success: false, message: "Couldn't Delete Book" });
+			}
+		} else {
+			res.json({ success: false, message: "Book ID not provided." });
+		}
+	}
+
+	async searchBook(req, res) {
+		const { q } = req.query;
+		if (q) {
+			const data = await bookModel.findAll({
+				where: {
+					[Op.or]: {
+						name: {
+							[Op.like]: `%${q}%`,
+						},
+						author: {
+							[Op.like]: `%${q}%`,
+						},
+					},
+				},
+			});
+			console.log(data);
+			res.json(data);
+		} else
+			res.json({ success: false, message: "Empty Query Search String." });
+	}
+
+	async getBooks(req, res) {
+		let { limit } = req.query;
+		if (!limit) {
+			limit = 20;
+		}
+		const data = await bookModel.findAll({
+			limit: limit,
+		});
+		res.json(data);
+	}
 }
--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -7,31 +7,36 @@ let imageName;
 
 //image upload
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads");
-  },
-  filename: function (req, file, cb) {
-    imageName =
-      Date.now() +
-      "-" +
-      Math.round(Math.random() * 1e9) +
-      "-" +
-      file.originalname.trim();
-    cb(null, imageName);
-  },
+	destination: function (req, file, cb) {
+		cb(null, "public/uploads");
+	},
+	filename: function (req, file, cb) {
+		imageName =
+			Date.now() +
+			"-" +
+			Math.round(Math.random() * 1e9) +
+			"-" +
+			file.originalname.trim();
+		cb(null, imageName);
+	},
 });
 
 const upload = multer({ storage });
 const bookController = new BookController();
 
 router.post("/add", upload.single("image"), (req, res) => {
-  bookController.addBook(req, res, imageName);
+	bookController.addBook(req, res, imageName);
 });
 
 router.get("/:id", bookController.getBookByID);
 
+//?limit=20
+router.get("/", bookController.getBooks);
+
 router.put("/update/:id", bookController.updateBook);
 
 router.delete("/delete/:id", bookController.deleteBook);
+
+router.get("/search/all", bookController.searchBook);
 
 export default router;
